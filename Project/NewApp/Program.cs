07f378d6f93370cc1b6bda3341fccade81c595bd
TranThiMaiAnh <tranthimaianh01112002@gmail.com>
2023-09-07 18:53:21 +0700

    for

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,19 +4,13 @@ public class Program
     private static void Main(string[]args)
     {
 
-int a,b;
-      System.Console.Write("a = ");
-       a = Convert.ToInt32(Console.ReadLine());
-
-      System.Console.Write("b = ");
-       b = Convert.ToInt32(Console.ReadLine());
-       
-
-        System.Console.WriteLine("{0} - {1} = {2}",a,b,a-b);
-        System.Console.WriteLine("{0} * {1} = {2}",a,b,a*b);
-        System.Console.WriteLine("{0} / {1} = {2}",a,b,a/b);
-        System.Console.WriteLine("{0} % {1} = {2}",a,b,a%b);
-   }
+      //lặp for
+        for(int i = 1; i <=9; i ++)
+        {
+          System.Console.WriteLine("Number {0}" ,i);
+        }
+      }
+    
    
 }
 
