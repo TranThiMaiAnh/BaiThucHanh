79183ed1d3ce35636fa7da2a3998cf956b53f984
TranThiMaiAnh <tranthimaianh01112002@gmail.com>
2023-09-07 17:02:13 +0700

    C# BASIC

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,2 +1,124 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[]args)
+    {
+//kkhai báo kiểu biến số
+    //     //khai báo biến a kiểu số nguyên
+    //     int a = 5;
+    //     Console.WriteLine(a);
+    //     //khai báo kiểu số thực
+    //     double b = 1.5;
+    //     Console.WriteLine(b);
+    //     //khai báo kiẻu chuỗi
+    //     string name ="Tran Thi Mai Anh";
+    //     Console.WriteLine(name);
+    //     //khai báo kiểu ký tự
+    //     char kytu ='a';
+    //     Console.WriteLine(kytu);
+    //     //khai báo kiểu bool
+    //     bool d = true;
+    //     bool e = false;
+    //     Console.WriteLine(d);
+    //     Console.WriteLine(e);  
+
+// khai báo kiểu hằng số thêm từ khóa const trước biến số
+    // const string fullName = "Tran Thi Mai Anh";
+    // Console.WriteLine(fullName);
+// các toán tử
+    
+            // int a;
+            // int b;
+           
+            // //yêu cầu người dùng nhập vào số bị chia và số chia
+            // Console.Write("\n Nhap vao so a: ");
+            // a = Convert.ToInt32(Console.ReadLine());
+            // Console.Write("\n Nhap vao so b: ");
+            // b = Convert.ToInt32(Console.ReadLine());
+            // //hiển thị phép tính là phép chia giữa hai số
+            // Console.WriteLine("\n Hieu: {0} - {1} = ?", a, b);
+            // Console.WriteLine("\n Tich: {0} * {1} = ?", a, b);
+            // Console.WriteLine("\n chia lay dư: {0} / {1} = ?", a, b);
+            // int hieu = a - b;
+            // int tich = a * b;
+            
+            // //tinh phần dư của phép chia bằng toán toán tử %
+            //  phanDu =  a % b;
+            
+            
+            // //tính phần nguyên của phép chia bằng toán tử /
+            // int phanNguyen = a / b;
+            // //hiển thị kết quả ra màn hình console bao gồm phần nguyên và phần dư
+            // Console.WriteLine("\n hieu cua hai so la: " + hieu);
+            // Console.WriteLine("\n tich cua hai so la: " + tich);
+            // Console.WriteLine("\n Phan nguyen cua phep chia la: " + phanNguyen);
+            // Console.WriteLine("\n Phan du cua phep chia la: " + phanDu);
+// so sánh 2 số
+    //     int a,b;
+    //   System.Console.Write("a = ");
+    //    a = Convert.ToInt32(Console.ReadLine());
+      
+    //   System.Console.Write("b = ");
+    //    b = Convert.ToInt32(Console.ReadLine());
+
+    //   if(a<b){
+    //     Console.WriteLine("{0} nho hon {1}",a,b);
+    //   } else{
+    //     Console.WriteLine("{0} lon hon {1}",a,b);
+
+// câu lệnh IF-ELSE
+  //   int c = 10;
+  //   if(c<0)
+  //   {
+  //     System.Console.WriteLine("{0} là số nguyên âm", c);
+  //   }else{
+  //     if( c % 2 == 0) {
+  //       System.Console.WriteLine("{0} là số nguyên dương chẵn", c);
+  //     }else{
+  //       System.Console.WriteLine("{0} là số nguyên dưong lẻ", c);
+  //     }
+  //   }
+  // }
+// lệnh swich ... case
+  // int day = 8;
+  // switch(day){
+  //   case 1: System.Console.WriteLine("Sunday"); break;
+  //   case 2: System.Console.WriteLine("Monday"); break;
+  //   case 3: System.Console.WriteLine("Tuesday"); break;
+  //   case 5: System.Console.WriteLine("wednesday"); break;
+  //   case 6: System.Console.WriteLine("Friday"); break;
+
+  //   default:
+  //     System.Console.WriteLine("Không phai ngay trong tuan");
+  //     break;
+  //   }
+  // }
+// lặp for
+//   for(int i = 1; i <=9; i ++)
+//   {
+//     System.Console.WriteLine("Number {0}" ,i);
+//   }
+// }
+// lặp while
+//   int a = 2;
+//   while(a<6)
+//   {
+//     System.Console.WriteLine("Von lap thu {0}", a);
+//     a++;
+//   }
+// }
+// lạp do...while
+// 
+// break and continue
+for(int i = 0; i < 10; i++)
+{
+  //if(i == 3) break;
+  if(i == 3) continue;
+  System.Console.WriteLine("Vong lap thu {0}", i);
+}
+}
+}
+
+
+    
+// câu lệnh if-else
