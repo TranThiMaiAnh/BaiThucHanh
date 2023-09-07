e63d11d7d756175843eb10310f7034e9cb0cb4a8
TranThiMaiAnh <tranthimaianh01112002@gmail.com>
2023-09-07 17:39:55 +0700

    c# bien so

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,26 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-internal class Program
+public class Program
 {
     private static void Main(string[]args)
     {
-//kkhai báo kiểu biến số
-    //     //khai báo biến a kiểu số nguyên
-    //     int a = 5;
-    //     Console.WriteLine(a);
-    //     //khai báo kiểu số thực
-    //     double b = 1.5;
-    //     Console.WriteLine(b);
-    //     //khai báo kiẻu chuỗi
-    //     string name ="Tran Thi Mai Anh";
-    //     Console.WriteLine(name);
-    //     //khai báo kiểu ký tự
-    //     char kytu ='a';
-    //     Console.WriteLine(kytu);
-    //     //khai báo kiểu bool
-    //     bool d = true;
-    //     bool e = false;
-    //     Console.WriteLine(d);
-    //     Console.WriteLine(e);  
+//khai báo kiểu biến số
+       //khai báo biến a kiểu số nguyên
+       int msv = 2021050093;
+        Console.WriteLine(msv);
+     //khai báo kiểu số thực
+        double b = 1.5;
+      Console.WriteLine(b);
+     //khai báo kiẻu chuỗi
+     string name ="Tran Thi Mai Anh";
+     Console.WriteLine(name);
+     //khai báo kiểu ký tự
+     char kytu ='a';
+     Console.WriteLine(kytu);
+     //khai báo kiểu bool
+      bool d = true;
+      bool e = false;
+       Console.WriteLine(d);
+      Console.WriteLine(e);  
+    }
+}
 
 // khai báo kiểu hằng số thêm từ khóa const trước biến số
     // const string fullName = "Tran Thi Mai Anh";
@@ -110,15 +112,16 @@ internal class Program
 // lạp do...while
 // 
 // break and continue
-for(int i = 0; i < 10; i++)
-{
-  //if(i == 3) break;
-  if(i == 3) continue;
-  System.Console.WriteLine("Vong lap thu {0}", i);
-}
-}
-}
+// for(int i = 0; i < 10; i++)
+// {
+//   //if(i == 3) break;
+//   if(i == 3) continue;
+//   System.Console.WriteLine("Vong lap thu {0}", i);
+// }
+// }
+// }
 
 
     
 // câu lệnh if-else
+
