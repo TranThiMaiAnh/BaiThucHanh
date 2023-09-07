4230136356a679102a2f704d3bc932216dcbfdde
TranThiMaiAnh <tranthimaianh01112002@gmail.com>
2023-09-07 18:59:39 +0700

    continue

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,10 +4,12 @@ public class Program
     private static void Main(string[]args)
     {
 
-    //  break
+    //  continue
+    
+    
 for(int i = 0; i < 10; i++)
 {
-  if(i == 3) break;
+  if(i == 3) continue;
   
   System.Console.WriteLine("Vong lap thu {0}", i);
     
