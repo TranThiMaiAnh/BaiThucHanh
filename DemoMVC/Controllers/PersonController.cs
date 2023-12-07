1700ee11e7be81abd361e434e689662571ec3f62
TranThiMaiAnh <tranthimaianh01112002@gmail.com>
2023-12-07 20:03:42 +0700

    upload file excel

--- DemoMVC/Controllers/PersonController.cs
+++ DemoMVC/Controllers/PersonController.cs
@@ -7,17 +7,19 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DemoMVC.Data;
 using DemoMVC.Models;
-
+using DemoMVC.Models.Process;
 namespace DemoMVC.Controllers
 {
     public class PersonController : Controller
     {
+        //trân thi mai anh - 201050093
         private readonly ApplicationDbContext _context;
 
         public PersonController(ApplicationDbContext context)
         {
             _context = context;
         }
+        private ExcelProcess _excelPro = new ExcelProcess();
 
         // GET: Person
         public async Task<IActionResult> Index()
@@ -147,6 +149,53 @@ namespace DemoMVC.Controllers
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        //upload
+         public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file!=null)
+                {
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                    {
+                        ModelState.AddModelError("", "Please choose excel file to upload!");
+                    }
+                    else
+                    {
+                        //rename file when upload to server
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", "File" + DateTime.Now.Day + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Millisecond + fileExtension);
+                        var fileLocation = new FileInfo(filePath).ToString();
+                        if (file.Length > 0)
+                        {
+                            using (var stream = new FileStream(filePath, FileMode.Create))
+                            {
+                                //save file to server
+                                await file.CopyToAsync(stream);
+                                //read data from file and write to database
+                                var dt = _excelPro.ExcelToDataTable(fileLocation);
+                                for(int i = 0; i < dt.Rows.Count; i++)
+                                {
+                                    var ps = new Person();
+                                    ps.PersonID = dt.Rows[i][0].ToString();
+                                    ps.FullName = dt.Rows[i][1].ToString();
+                                    ps.Address = dt.Rows[i][2].ToString();
+
+                                    _context.Add(ps);
+                                }
+                                await _context.SaveChangesAsync();
+                                return RedirectToAction(nameof(Index));
+                            }
+                        }
+                    }
+                }
+
+            return View();
         }
 
         private bool PersonExists(string id)
